420eb083a2b0a6111dca8817a7318fb12cafeef7
FECEANMA <fernandoencalada921@gmail.com>
2023-11-23 08:27:04 -0500

    Initial commit

---
+++ taxi.js
@@ -0,0 +1,21 @@
+'use strict'
+let vehicle = {
+    make: 'Toyota',
+    model: 'Camry',
+    color: 'White',
+    licensePlate: 'ABC - 0123',
+    year: 2009,
+    isEnable: false
+};
+
+function validarTaxi(vehicle){
+if (taxi<=2009, taxi >=1999 ){
+    alert("La antiguedad del vehiculo es menor o igual a 10 años");
+}
+else{
+    alert("La antiguedad del vehculo supera mas de los diez años");
+}
+}
+
+let taxi = Number(prompt("Ingrese el año del vehiculo")); 
+let resultado = validarTaxi(taxi);
